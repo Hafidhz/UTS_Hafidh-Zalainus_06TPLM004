335592766897809bc3ff8dae51efbb65421e1ee5
Hafidhz <103982477+Hafidhz@users.noreply.github.com>
2022-04-19 12:47:59 +0700

    Add files via upload

---
+++ looping.py
@@ -0,0 +1,21 @@
+print ("perulangan for \n")
+ulang = 10 
+for i in range(ulang):
+    print(f"perulangan ke-{i}")
+
+print("\n list")
+item=['agramas','stj','haryanto']
+for isi in item:
+    print(isi)
+
+print ("\n perulangan while")
+jawab = 'ya'
+hitung = 0
+
+while(True):
+    hitung += 1
+    jawab = input("Ulang lagi? ")
+    if jawab == 'tidak':
+        break
+
+print(f"Total perulagan: {hitung}")
